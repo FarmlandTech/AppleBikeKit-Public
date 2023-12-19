d6e8fd07b5f0b72d860debb14607f5509f4a11b3
Jeff Chiu <jeff.chiu@farmland.tw>
2023-12-19 19:57:50 +0800

    新增 BikeKit 的最初始父類，並封裝成黑盒，以實現存取的限制。

---
+++ Sources/AppleBikeKitSourceCode/AppleBikeKitSourceCode.xcframework/ios-arm64_x86_64-maccatalyst/AppleBikeKitSourceCode.framework/Versions/A/Headers/AppleBikeKitSourceCode.h
@@ -0,0 +1,18 @@
+//
+//  AppleBikeKitSourceCode.h
+//  AppleBikeKitSourceCode
+//
+//  Created by Yves Tsai on 2023/12/19.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for AppleBikeKitSourceCode.
+FOUNDATION_EXPORT double AppleBikeKitSourceCodeVersionNumber;
+
+//! Project version string for AppleBikeKitSourceCode.
+FOUNDATION_EXPORT const unsigned char AppleBikeKitSourceCodeVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AppleBikeKitSourceCode/PublicHeader.h>
+
+
